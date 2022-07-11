2b385259bc1468b3431c7761f832c7f513da1fc4
uploads <85631165+21Turtles@users.noreply.github.com>
2022-07-11 12:12:41 -0400

    Update ps2.js

--- src/ps2.js
+++ src/ps2.js
@@ -1,1 +1,6 @@
-
+var imported = document.createElement('script');
+imported.src = './load_1.js';
+document.head.appendChild(imported);
+var imported = document.createElement('script');
+imported.src = './load_2.js';
+document.head.appendChild(imported);
