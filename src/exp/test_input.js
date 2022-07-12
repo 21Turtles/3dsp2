da59a3d6a4a0474b3370d0e4ae35eca611f80b23
uploads <85631165+21Turtles@users.noreply.github.com>
2022-07-12 12:59:43 -0400

    Update test_input.js

--- src/exp/test_input.js
+++ src/exp/test_input.js
@@ -1,19 +1,22 @@
 while(true)
 {
   var inp = input.checkHeld();
-  if(inp == input.A) console.log("A pressed");
-  if(inp == input.A) console.log("A pressed"); 
-  if(inp == input.A) console.log("A pressed"); 
-  if(inp == input.A) console.log("A pressed"); 
-  if(inp == input.A) console.log("A pressed"); 
-  if(inp == input.A) console.log("A pressed");
-  if(inp == input.A) console.log("A pressed"); 
-  if(inp == input.A) console.log("A pressed"); 
-  if(inp == input.A) console.log("A pressed");
-  if(inp == input.A) console.log("A pressed");
-  if(inp == input.A) console.log("A pressed");
-  if(inp == input.A) console.log("A pressed");
-  if(inp == input.A) console.log("A pressed")
+  // Basic Controls
+  if(inp == input.A)                   ;
+  if(inp == input.B)                   ;
+  if(inp == input.X)                   ;
+  if(inp == input.Y)                   ;
+  if(inp == input.Select)                   ;
+  if(inp == input.Start)                   ;
+  if(inp == input.L)                   ;
+  if(inp == input.R)                   ;
+  if(inp == input.ZL)                   ;
+  if(inp == input.ZR)                   ;
+  // Now for the Dpad(s)
+  if(inp == input.A)                   ;
+  if(inp == input.A)                   ;
+  if(inp == input.A)                   ;
+  if(inp == input.A)                   ;
   
   
   
