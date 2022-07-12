01846d1f5c63e5bbf23800cd3e063e3264e597b7
uploads <85631165+21Turtles@users.noreply.github.com>
2022-07-12 10:47:27 -0400

    Update test_input.js

--- src/exp/test_input.js
+++ src/exp/test_input.js
@@ -1,1 +1,22 @@
-
+while(true)
+{
+  var inp = input.checkHeld();
+  if(inp == input.A) console.log("A pressed"); // preforms action if a key is pressed current action=print A pressed!"
+  if(inp == input.A) console.log("A pressed"); // preforms action if a key is pressed current action=print A pressed!"
+  if(inp == input.A) console.log("A pressed"); // preforms action if a key is pressed current action=print A pressed!"
+  if(inp == input.A) console.log("A pressed"); // preforms action if a key is pressed current action=print A pressed!"
+  if(inp == input.A) console.log("A pressed"); // preforms action if a key is pressed current action=print A pressed!"
+  if(inp == input.A) console.log("A pressed"); // preforms action if a key is pressed current action=print A pressed!"
+  if(inp == input.A) console.log("A pressed"); // preforms action if a key is pressed current action=print A pressed!"
+  if(inp == input.A) console.log("A pressed"); // preforms action if a key is pressed current action=print A pressed!"
+  
+  
+  
+  
+  
+  
+  
+  
+  else console.log("No key pressed"); //if no keys are pressed it will say "No key pressed"
+  console.clear(); // clears the console, may cause lag
+}
