1d3155e32b70566b5e9821145ac964915133ebaa
uploads <85631165+21Turtles@users.noreply.github.com>
2022-07-12 16:35:03 -0400

    Update test_input.js

--- src/exp/test_input.js
+++ src/exp/test_input.js
@@ -12,11 +12,21 @@ while(true)
   if(inp == input.R)                   ;
   if(inp == input.ZL)                   ;
   if(inp == input.ZR)                   ;
-  // Now for the Dpad(s)
-  if(inp == input.A)                   ;
-  if(inp == input.A)                   ;
-  if(inp == input.A)                   ;
-  if(inp == input.A)                   ;
+  // Now for the Dpad(s) Right Analong
+  if(inp == input.DRight)                   ;
+  if(inp == input.DLeft)                   ;
+  if(inp == input.DUp)                   ;
+  if(inp == input.DDown)                   ;
+  // Circle Pad  Right Analong
+  if(inp == input.CPRight)                   ;
+  if(inp == input.CPLeft)                   ;
+  if(inp == input.CPUp)                   ;
+  if(inp == input.CPDown)                   ;
+  // C-Stick for left Analog
+   if(inp == input.CSRight)                   ;
+  if(inp == input.CSLeft)                   ;
+  if(inp == input.CSUp)                   ;
+  if(inp == input.CSDown)                   ;
   
   
   
@@ -24,7 +34,6 @@ while(true)
   
   
   
-  
-  else console.log("No key pressed"); //if no keys are pressed it will say "No key pressed"
+  else console.log("Try pressing keys"); //if no keys are pressed it will say "No key pressed"
   console.clear(); // clears the console, may cause lag
 }
